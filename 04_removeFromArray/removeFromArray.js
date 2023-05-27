94f605dd9ae9fbc9d4a8a939d014a79d6cd53377
fraqui13 <feq113@protonmail.com>
2023-05-26 22:35:18 -0400

    add removeFromArray solution to repository

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,22 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(origArr, ...deleteMe) {
+    for (let i = 0; i < deleteMe.length; i++){
+        if(origArr.indexOf(deleteMe[i]) > -1){
+            origArr.splice(origArr.indexOf(deleteMe[i]), 1);
+        }   
+    }
+    return origArr;    
 };
+
+
+
+// removeFromArray([1, 2, 3, "cow", 4, 5, "dog", true, 2.555, 1], "dog");
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+/*const removeFromArray = function(origArr, ...deleteMe) {
+    origArr.splice(origArr.indexOf(deleteMe[0]), deleteMe.length);
+    console.log(origArr);
+    
+}*/
